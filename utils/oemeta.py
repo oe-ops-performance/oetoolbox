e45892479016b081861859a6cca7bfb4e844aa91
Brian Gruning <Brian.Gruning@corp.onwardenergy.com>
2025-01-27 18:24:06 -0500

    Add oemeta.py; re-write using pathlib & lambda function

---
+++ utils/oemeta.py
@@ -0,0 +1,12 @@
+import json
+from pathlib import Path
+from .utils import oepaths
+
+# get list of all JSON data files in python projects 'references' folder
+json_fpaths = [fp for fp in oepaths.references.glob("*.json") if "-DEN-" not in fp.name]
+
+# function to get file reference id from filename
+file_id = lambda fp: fp.stem.replace("MetaData_", "").replace("Structure_", "")
+
+# create reference dictionary
+data = {file_id(fp): json.load(open(fp)) for fp in json_fpaths}
