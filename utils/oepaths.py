7434a00cb7d02979a7e19295a5ae80744a5e5bc4
Brian Gruning <Brian.Gruning@corp.onwardenergy.com>
2026-03-13 14:50:36 -0400

    Update kpi tracker path to Rev2 file; Add sort option to get_contents func; Add new func for printing contents

--- utils/oepaths.py
+++ utils/oepaths.py
@@ -51,7 +51,7 @@ meter_generation_historian = Path(
     UTILITY_METER_DIR, "Master_Version", "Meter_Generation_Historian.xlsm"
 )
 kpi_tracker = Path(solar, "z_Monthly KPI Tracker.xlsx")
-kpi_tracker_rev1 = Path(solar, "z_Monthly KPI Tracker_Rev1.xlsx")
+kpi_tracker_rev1 = Path(solar, "z_Monthly KPI Tracker_Rev2.xlsx")
 kpi_notes_file = Path(flashreports, "_kpis_and_notes_.xlsx")
 
 
@@ -80,9 +80,15 @@ def validated_savepath(fpath):
 
 
 # function to sort filepath list by date created
-def sorted_filepaths(filepath_list):
+def sorted_filepaths(filepath_list, sortby="date_created"):
     """sorts list of pathlib.Path filepaths by date created (most recent first)"""
-    sort_kwargs = dict(key=lambda fp: fp.stat().st_ctime, reverse=True)
+    if sortby not in ("date_created", "date_modified"):
+        raise ValueError(f"{sortby=} is not supported.")
+    sort_keys = {
+        "date_created": lambda fp: fp.stat().st_ctime,
+        "date_modified": lambda fp: fp.stat().st_mtime,
+    }
+    sort_kwargs = dict(key=sort_keys[sortby], reverse=True)
     return list(sorted(filepath_list, **sort_kwargs))
 
 
@@ -94,11 +100,11 @@ def latest_file(filepath_list):
     return sorted_filepaths(filepath_list)[0]
 
 
-def get_contents(folder, only_names=False):
+def get_contents(folder, only_names=False, sortby="date_created"):
     if not Path(folder).is_dir() or not Path(folder).exists():
         raise ValueError("Provided path is not a valid directory.")
     output = dict(files=[], folders=[], other=[])
-    for fp in sorted_filepaths(list(Path(folder).glob("*"))):
+    for fp in sorted_filepaths(list(Path(folder).glob("*")), sortby=sortby):
         if fp.is_dir():
             key = "folders"
         elif fp.is_file():
@@ -108,3 +114,36 @@ def get_contents(folder, only_names=False):
         item = fp.name if only_names is True else fp
         output[key].append(item)
     return output
+
+
+def print_directory_contents(directory, folders=True, other=False, sortby="date_created"):
+    try:
+        contents = get_contents(directory, only_names=False, sortby=sortby)
+    except ValueError as err:
+        print(f"Error with directory or sortby args: {err}\nExiting.")
+        return
+    except Exception as e:
+        print(f"Unknown error: {e}\nExiting.")
+        return
+
+    include_keys = ["files"]
+    if folders is True:
+        include_keys.append("folders")
+    if other is True:
+        include_keys.append("other")
+
+    def get_date(filepath):
+        kwargs = dict(filepath=filepath, as_string=True)
+        if sortby == "date_created":
+            return date_created(**kwargs)
+        return date_modified(**kwargs)
+
+    print(f"Contents of directory: <{str(directory)}>")
+    for key, fplist in contents.items():
+        if key not in include_keys:
+            continue
+        print(f"\n{key}:")
+        for fp in fplist:
+            print(f"{get_date(fp)} >>> {fp.name}")
+
+    return
